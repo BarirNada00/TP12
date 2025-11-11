d54a28508082efcfd2056efbcb73088097f60dee
BarirNada00 <116973510+BarirNada00@users.noreply.github.com>
2025-11-11 21:21:58 +0100

    first commit

---
+++ src/main/java/com/example/acme/cxf/Server.java
@@ -0,0 +1,15 @@
+package com.example.acme.cxf;
+
+import com.example.acme.cxf.impl.HelloServiceImpl;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+
+public class Server {
+    public static void main(String[] args) {
+        String address = "http://localhost:8080/services/hello";
+        JaxWsServerFactoryBean factory = new JaxWsServerFactoryBean();
+        factory.setServiceClass(HelloServiceImpl.class);
+        factory.setAddress(address);
+        factory.create();
+        System.out.println("WSDL: " + address + "?wsdl");
+    }
+}
